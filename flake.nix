e1ab94b5a66b47631e7a07197b1f01da71946e8c
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-10-16 21:33:18 +0300

    Add base go flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Go dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              # Dev Env
+              git
+              neovim
+              fd
+              fzf
+              ripgrep
+              fish
+              starship
+              bat
+              # Go deps
+              go
+              gotools # Go tools like goimports, godoc, and others
+              golangci-lint
+            ];
+
+            shellHook = ''
+              exec ${fish.outPath}/bin/fish
+            '';
+          };
+      }
+    );
+}
